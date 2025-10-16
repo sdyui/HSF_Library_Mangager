642fbb0c4e0804b980bbbdab9fede0b90eee3669
sdyui <quanghuy1472005@gmail.com>
2025-10-16 15:27:00 +0700

    abc
    hehheheheheh

---
+++ hsf/src/main/java/com/example/hsf_library_manager/HsfLibraryManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.hsf_library_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsfLibraryManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HsfLibraryManagerApplication.class, args);
+    }
+
+}
